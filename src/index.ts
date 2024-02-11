58cdc2087748cf5d5a154f5c6c532eb85245804b
Pavel Dalhou <dalhoupavel@gmail.com>
2024-02-11 23:38:35 +0100

    feat: add addUser function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import http from "http";
-import {getUser} from "./modules/getUsers"
+import { getUser } from "./modules/getUsers"
+import { addUser } from "./modules/addUser"
 
 const PORT = 4000;
 
 const mamontServer = http.createServer((request, response) => {
   if (request.method == "GET" && request.url == "/api/users") {
     return getUser(request, response);
-  }
+  } else if (request.method == "POST" && request.url == "/api/users") {
+    return addUser(request, response);
+  } 
 })
 
 mamontServer.listen(4000, () => {
--- src/modules/addUser.ts
+++ src/modules/addUser.ts
@@ -5,3 +5,52 @@ import { ServerResponse, IncomingMessage } from "http";
 
 import { User } from "./userInterface";
 
+const addUser = (request:IncomingMessage,  response:ServerResponse) => {
+  let data : string = "";
+
+  request.on('data', (chunk) => {
+    data += chunk.toString();
+  })
+
+  request.on("end", () => {
+    let user = JSON.parse(data);
+
+    fs.readFile(path.join(__dirname, "..", "database.json"), "utf8", (err, data) => {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "application/json"})
+        response.end(
+          JSON.stringify({
+            success: false,
+            error: err
+          })
+        )
+      } else {
+        let users: Array<User> = JSON.parse(data)
+        let latest_id = users.reduce(
+          (max = 0, task: User) => (task.id > max ? task.id : max),
+          0
+        );
+        user.id = latest_id + 1;
+        users.push(user);
+
+        fs.writeFile(path.join(__dirname, "..", "database.json"), JSON.stringify(users), (err) => {
+          if(err) {
+            response.writeHead(500, {"Content-Type": "application/json"})
+            response.end(JSON.stringify({
+              seccess: false,
+              error: err,
+            }))
+          } else {
+            response.writeHead(200, {"Content-Type": "application/json"})
+            response.end(JSON.stringify({
+              success: true,
+              message: user
+            }))
+          }
+        })
+      }
+    })
+  })
+}
+
+export { addUser }
